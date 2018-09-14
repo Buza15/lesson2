3f0213ce3928306945129ce498b680877efc0d0c
Andrew <Andrew>
2018-09-14 17:04:45 +0300

    task2

--- homework.py
+++ homework.py
@@ -1,11 +1,31 @@
 def task1():
     pass
-    
+
+a = int(input("Enter a number: "))
+c = input("choose into (b) or into (k): ")
+
+if c == 'b':
+    print("%d Байт = %d Килобайт" % (a, a*1024))
+elif c == 'k':
+    print("%d Килобайт = %d Байт" % (a, a/1024))
+
+print('#####' * 10)
+
     
 def task2():
     pass
     
-    
+n = int(input("Enter a number: "))
+s = 0
+m = 1
+while n > 0:
+    s += n%10
+    m *= n%10
+    n = n//10
+
+print("Summ: ", s)
+print ("Multiply", m)
+
 def task3():
     pass
 
